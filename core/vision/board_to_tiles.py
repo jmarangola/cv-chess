67069f9fbf786d1e2fce86f439dd23703eb2bb8b
fabiobc3 <65378660+fabiobc3@users.noreply.github.com>
2021-11-01 13:23:34 -0400

    Update board_to_tiles.py

--- core/vision/board_to_tiles.py
+++ core/vision/board_to_tiles.py
@@ -1,3 +1,6 @@
 """
-Returns 64 images, one of each individual board tile after recieving the image of an entire board. 
+Returns 64 images, one of each individual board tile after recieving the image of an entire board.
+
+Input: Image of a chess board.
+Return: 64 images, each of a single tile.  
 """
