745f2ca7f49eb86f503924c9dd29ecc453b960a0
fabiobc3 <65378660+fabiobc3@users.noreply.github.com>
2021-11-01 18:03:29 -0400

    Update and rename is_tile_empty.py to what_in_tile.py

--- core/vision/is_tile_empty.py
+++
@@ -1,6 +0,0 @@
-"""
-Returns True if the given tile is empty, and false if it contains a piece.
-
-Input: image of a sigle tile
-Return: True if tile is empty.
-"""
---
+++ core/vision/what_in_tile.py
@@ -0,0 +1,6 @@
+"""
+Returns "Black", "White" or "Empty" given a tile.
+
+Input: image of a sigle tile
+Return: "Black", "White" or "Empty" according to what is in the board. 
+"""
