23b7a811b2d9880801bb5a7ac55e53731f02bf33
fabiobc3 <65378660+fabiobc3@users.noreply.github.com>
2021-11-01 13:22:54 -0400

    Update is_tile_empty.py

--- core/vision/is_tile_empty.py
+++ core/vision/is_tile_empty.py
@@ -1,3 +1,6 @@
 """
-Return True if the given tile is empty, and false if it contains a piece.
+Returns True if the given tile is empty, and false if it contains a piece.
+
+Input: image of a sigle tile
+Returns: True if tile is empty.
 """
