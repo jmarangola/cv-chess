95dc61e775f5aee6c3ea1ec2fd034188dca262b1
John Marangola <john.marangola@gmail.com>
2021-11-13 00:42:01 -0500

    Preprocessing stubs

---
+++ core/vision/preprocessing.py
@@ -0,0 +1,21 @@
+"""
+Handle preprocessing for chessboard
+"""
+
+"""
+Extract the qr codes w/ coordinates from the image, returning a dictionary of label: {corner coordinates} pairs
+"""
+def extract_qr(img):
+    pass # TODO
+
+"""
+Return resized img to size resolution
+"""
+def resize(img, resolution=(800, 800)):
+    pass # TODO
+
+"""
+Return the cropped image at the four corners, 
+"""
+def crop_and_div(img, img_corners):
+    pass # TODO
